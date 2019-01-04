882b6fbe9e0fd2a9e21bc1e2b5b025695d0a7d1c
guoxing <guoxing@tujia.com>
2019-01-04 16:56:05 +0800

    增加组件缓存

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-     <div v-if="topicsList" class="topics-list">
-      <div v-for="item in topicsList" :key="item.id" class="topics-list__item" @click="navDetail(item)">
-        <h3>{{item.title}}</h3>
-        <span>{{item.create_at}}</span>
-      </div>
+    <img v-if="isMounted" alt="Vue logo" src="../assets/logo.png">
+    <div v-if="topicsList" class="topics-list">
+      <topic-item v-for="item in topicsList" :key="item.id" :topicInfo="item" @click="navDetail(item)"/>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import TopicItem from '../components/TopicItem'
 // @ is an alias to /src
 export default {
   name: "home",
+  components: { TopicItem },
+  data(){
+    return{
+      isMounted:false //for control not ssr render
+    }
+  },
   computed:{
     ...mapState(['topicsList'])
   },
   asyncData({ store, route: { params, query, fullPath }, cookies, userAgent }) {
     return store.dispatch('FETCH_TOPICS_LIST')
+  },
+  mounted(){
+    this.isMounted = true
   },
   methods:{
     navDetail(detail){
@@ -32,16 +39,6 @@ export default {
 .topics-list{
   max-width: 500px;
   margin: 0 auto;
-  .topics-list__item{
-    padding: 10px;
-    border-bottom: 1px solid #999;
-    &:hover{
-      background: #eee;
-    }
-    h3{
-      margin: 0;
-    }
-  }
 }
 </style>
 
