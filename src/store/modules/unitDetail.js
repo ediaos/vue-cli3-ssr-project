fb34f7983af31c7e58009e246efb91a921274962
guoxing <guoxing@tujia.com>
2019-01-08 21:28:20 +0800

    完成 module 功能

---
+++ src/store/modules/unitDetail.js
@@ -0,0 +1,27 @@
+/**
+ * 详情页store
+ */
+import { fetchUnitDetail } from "@/api/unitApi";
+export default {
+  namespaced: true,
+  // 重要信息：state 必须是一个函数，
+  // 因此可以创建多个实例化该模块
+  state: () => ({
+    apiData: {}
+  }),
+  actions: {
+    // eslint-disable-next-line
+    FETCH_UNIT_DETAIL: ({ commit }, params = { unitId, fromSource, cookies }) => {
+      return fetchUnitDetail(params)
+        .then(data => commit("SET_UNIT_DETAIL", { isSuccess: true, data }))
+        .catch(err => {
+          commit("SET_UNIT_DETAIL", { isSuccess: false, err });
+        });
+    }
+  },
+  mutations: {
+    SET_UNIT_DETAIL: (state, data) => {
+      state.apiData = data || {};
+    }
+  }
+}
