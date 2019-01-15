7db9889ae854b489036d0601de87d3fb7acea4cf
guoxing <guoxing@tujia.com>
2019-01-15 17:59:01 +0800

    fix cookie bug

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
+const fs = require('fs')
 const path = require('path')
 const Koa = require('koa')
 const static = require('koa-static')
-const cookie = require('koa-cookie').default;
-const mount = require('koa-mount');
+const cookie = require('koa-cookie').default
+const mount = require('koa-mount')
 const LRU = require('lru-cache')
 const morgan = require('koa-morgan')
 const resolve = (file) => path.resolve(__dirname, file)
@@ -11,8 +11,8 @@ const isServerRenderPage = require('./ssr-page-config')
 const PORT = process.env.NODE_PORT ? parseInt(process.env.NODE_PORT) : 8080
 
 const isDev = process.env.NODE_ENV === 'dev'
-const template = fs.readFileSync(resolve("./index.template.html"), "utf-8")
-const spaTemplate = fs.readFileSync(resolve(`${ isDev ? '../static/index.html': '../index.html' }`), "utf-8")
+const template = fs.readFileSync(resolve('./index.template.html'), 'utf-8')
+const spaTemplate = fs.readFileSync(resolve(`${isDev ? '../static/index.html' : '../index.html'}`), 'utf-8')
 const { createBundleRenderer } = require('vue-server-renderer')
 const setupServer = require(`${isDev ? './setup-dev-server' : './setup-prod-server'}`)
 
@@ -22,7 +22,7 @@ app.use(cookie())
 
 // 获取render
 let renderer
-setupServer(app,(bundle, options)=>{
+setupServer(app, (bundle, options) => {
   renderer = createBundleRenderer(
     bundle,
     Object.assign(options, {
@@ -45,18 +45,18 @@ async function ssrRequestHandle(ctx, next) {
   const context = {
     title: 'SSR PAGE', // default title
     url: ctx.url,
-    cookies: ctx.cookie, // for cookie using
+    cookies: ctx.cookie || {}, // for cookie using
     userAgent: ctx.header['user-agent']
   }
-  
+
   try {
     ctx.body = await renderer.renderToString(context)
   } catch (err) {
-    handleError(ctx,err)
+    handleError(ctx, err)
   }
 }
 
-const handleError = (ctx,err) => {
+const handleError = (ctx, err) => {
   if (err.url) {
     ctx.status = err.code || 302
     ctx.redirect(err.url)
@@ -68,33 +68,31 @@ const handleError = (ctx,err) => {
     ctx.status = 500
     ctx.body = '500 | Internal Server Error'
   }
-  console.error(`error during render : url=${ctx.url} err=`,err)
+  console.error(`error during render : url=${ctx.url} err=`, err)
 }
 
 //开放dist目录
-app.use(mount('/static',static(resolve('../static'))))
+app.use(mount('/static', static(resolve('../static'))))
 
 // setup the access logger
-if(!isDev){
+if (!isDev) {
   app.use(morgan('combined'))
 }
 
 // 处理请求
-app.use(async(ctx,next)=>{
-  if(isServerRenderPage(ctx,ctx.cookie)){
-    await ssrRequestHandle(ctx,next)
-  }
-  else{
+app.use(async (ctx, next) => {
+  if (isServerRenderPage(ctx, ctx.cookie || {})) {
+    await ssrRequestHandle(ctx, next)
+  } else {
     ctx.body = spaTemplate
   }
 })
 
-app.listen(PORT)
-  .on('listening',()=>{
-    console.log(`server started at localhost:${PORT}`);
+app
+  .listen(PORT)
+  .on('listening', () => {
+    console.log(`server started at localhost:${PORT}`)
   })
-  .on('error',(err)=>{
-    console.log('---server error---',err)
+  .on('error', (err) => {
+    console.log('---server error---', err)
   })
-
-
